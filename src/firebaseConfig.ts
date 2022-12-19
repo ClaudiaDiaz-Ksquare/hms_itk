37d1f1b89161a6b083581055ffca78b124dc3f17
“Dannaé <claudia.diaz@itksquare.edu.mx>
2022-12-19 01:12:57 -0600

    Installed packages

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBX808q3CdRWE0OY_8lyHsfi6VaoDdJBY0",
+    authDomain: "hms-itk-60621.firebaseapp.com",
+    projectId: "hms-itk-60621",
+    storageBucket: "hms-itk-60621.appspot.com",
+    messagingSenderId: "398536666401",
+    appId: "1:398536666401:web:9aaea30da6c3527edee260",
+    measurementId: "G-LQZW46BP7C"
+  };
